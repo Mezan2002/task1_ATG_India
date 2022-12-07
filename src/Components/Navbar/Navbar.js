e87dba01f749d70b9269f2aa6410ae0703a4ac4a
Mezan2002 <mezanurr009@gmail.com>
2022-12-08 00:52:16 +0600

    Navbar done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,36 @@
 import React from "react";
 import logo from "../../logo.svg";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar px-5">
+      <nav className="navbar">
         <div className="container-fluid">
           <a href="/" className="navbar-brand">
             <img src={logo} alt="" />
           </a>
-          <form className="d-flex" role="search">
-            <input type="search" name="" id="" />
+          <form className="d-flex position-relative" role="search">
+            <span>
+              <i class="fa-solid fa-magnifying-glass searchIcon"></i>
+            </span>
+            <input
+              type="search"
+              name=""
+              id=""
+              placeholder="Search for your favorite groups in ATG"
+              className="searchField"
+            />
           </form>
-          <button className="btn btn-outline-success" type="submit">
-            Create account. It's free!
-          </button>
+          <div className="d-flex" type="submit">
+            <p>Create account.</p>
+            <p className="createAccountBtn">
+              It's free!
+              <span className="miniArrowBtn">
+                <i class="fa-sharp fa-solid fa-caret-down"></i>
+              </span>
+            </p>
+          </div>
         </div>
       </nav>
     </div>
